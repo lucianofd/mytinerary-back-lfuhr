9882c848223d62935e59f5a3bdb84b5c1311ecb8
lucianofd <73968897+lucianofd@users.noreply.github.com>
2023-08-19 12:46:21 -0300

    Update cityController.js

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -1,7 +1,7 @@
 import Cities from'./CityModel.js';
 
 const cityController{
- getCities = async (req, res, next) => {
+ getCities: async (req, res, next) => {
   try {
     const cities = await Cities.find();
     res.status(200).json(cities);
@@ -10,17 +10,50 @@ const cityController{
     }
   };
 
- getOneCity =  async (req, res, next) => {
+ getOneCity : async (req, res, next) => {
    const { name } = req.params;
    try {
     const city = await cities.find(city => city.name.toLowerCase() == name.toLowerCase());
     res.status(200).json(city);
   } catch (e) {
     res.status(500).send('Error inesperado ' + e);
-    }
+    } 
   };
 
-  
+  postCity : async(req, res, next) => {
+     try { nombre , description, country } = req.body
+
+      if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
+      res.status(413).send("El campo nombre no puede estar vacío");
+    } else if  (
+      pais == null ||
+      pais.length == 0 ||
+      /^\s+$/.test(pais)
+    ) {
+      res.status(413).send("El campo pais no puede estar vacio");
+    }
+      else {
+      const existCity = await cities.find(city => city.name.toLowerCase() == name.toLowerCase());
+
+      if (existCity) {
+        res.status(413).send('La ciudad ya existe en la base');
+      } else {
+        const newCity = new City ({
+          nombre,
+          description,
+         country,
+        });
+      const savedCity = await newCity.save();
+
+        res.status(200).json(savedPersona);
+      }
+    } 
+     
+      catch (e) {
+    res.status(500).send('Error inesperado ' + e);
+  }
+   
+  } ; 
   
 
 }
