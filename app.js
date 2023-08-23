fff2436e61026e251646cbf6467adcb8d1f0ffb9
lucianoFD <dlffuhr@gmail.com>
2023-08-23 17:51:38 -0300

    paquetes

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('api/cities', indexRouter);
-app.use('api/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
